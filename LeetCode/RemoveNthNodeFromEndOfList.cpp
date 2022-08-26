3c0de17a0d511631e418f737c4ba17e5768a71e2
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-08-26 23:52:08 +0300

    Create RemoveNthNodeFromEndOfList.cpp

---
+++ LeetCode/RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,48 @@
+
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        vector<pair<int,ListNode*>> v;
+        ListNode *ptr = head;
+        while (head != nullptr) {
+            v.push_back({head->val, head});
+            head = head->next;
+        }
+  
+        auto index = v.size() - n;
+        
+        auto it = v.begin() + index;
+        v.erase(it);
+        
+        if (v.size() != 0) {
+            ptr = v[0].second;
+        } else {
+            return nullptr;
+        }
+        ListNode *res = ptr;
+        decltype(v.size()) i = 0;
+        while (i != v.size()) {
+            if (i == v.size() - 1) {
+                *ptr = ListNode(v[i].first);
+            } else {
+                *ptr = ListNode(v[i].first, v[i + 1].second);
+                ptr = ptr->next;
+            }
+            ++i;
+        }
+        return res;                
+    }
+};
+
+int main() {
+  
+  return 0; 
+}
