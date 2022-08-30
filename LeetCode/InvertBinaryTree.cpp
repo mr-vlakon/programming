d307730a7ee246c6610393854798df2826e55d9d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-08-30 17:36:11 +0300

    Create InvertBinaryTree.cpp

---
+++ LeetCode/InvertBinaryTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:    
+    void Invert(TreeNode * root) {
+        if (root == nullptr) return;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        Invert(root->right);
+        Invert(root->left);
+    }
+    
+    TreeNode* invertTree(TreeNode* root) {
+        Invert(root);
+        return root;
+    }
+};
+
+int main() {
+  
+  
+  return 0;  
+}
