6e9e4aadad4bd712f85036ba6608871449fa981f
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-08-30 14:30:28 +0300

    Create FindSmallestLetterGreaterThanTarget.cpp

---
+++ LeetCode/FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        auto it = upper_bound(letters.begin(), letters.end(), target);
+        if (it == letters.end()) return *letters.begin();
+        return *it;
+    }
+};
+
+int main() {
+
+  return 0;
+}
