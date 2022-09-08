75020d483736ee2dc3111e24e851856693d1090d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-09-08 20:04:14 +0300

    Create MaximumProductOfTwoElementsInAnArray.cpp

---
+++ LeetCode/MaximumProductOfTwoElementsInAnArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        auto it = max_element(nums.cbegin(), nums.cend());
+        int first = *it - 1;
+        nums.erase(it);
+        it = max_element(nums.cbegin(), nums.cend());
+        int second = *it - 1;
+        return first * second;
+    }
+};
+
+int main() {
+  
+  
+  return 0;  
+}
