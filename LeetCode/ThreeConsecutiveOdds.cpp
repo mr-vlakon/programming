49d09f8eefc5c93bb8fddfe700aa6a5e53b06033
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-09-08 19:51:57 +0300

    Create ThreeConsecutiveOdds.cpp

---
+++ LeetCode/ThreeConsecutiveOdds.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if (arr.size() <= 2) return false;
+        for (decltype(arr.size()) i = 0; i != arr.size() - 2; ++i) {
+            if ((arr[i] % 2 == 1) && (arr[i + 1] % 2 == 1) && (arr[i + 2] % 2 == 1))
+                return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
