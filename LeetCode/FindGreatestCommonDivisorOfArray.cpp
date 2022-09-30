d5bd36ecee2a337b8d806314073d86c610e3070e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-09-30 15:30:43 +0300

    Create FindGreatestCommonDivisorOfArray.cpp

---
+++ LeetCode/FindGreatestCommonDivisorOfArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return gcd(nums[0], nums.back());
+    }
+};
+
+int main() {
+  
+  return 0;  
+}
