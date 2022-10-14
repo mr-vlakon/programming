459611d23304f64374bb9f1fca69467dd0d1853a
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-10-14 19:02:38 +0300

    Update K-ConcatentionMaximumSum.cpp

--- LeetCode/K-ConcatentionMaximumSum.cpp
+++ LeetCode/K-ConcatentionMaximumSum.cpp
@@ -3,7 +3,7 @@ public:
     long long kadane(const vector<int> &v) {
         long long max_so_far = INT_MIN, max_ending_here = 0;
 
-        for (int i = 0; i < v.size(); i++) {
+        for (decltype(v.size()) i = 0; i < v.size(); i++) {
             max_ending_here = max_ending_here + v[i];
             if (max_so_far < max_ending_here) {
                 max_so_far = max_ending_here;
@@ -13,44 +13,38 @@ public:
                 max_ending_here = 0;
         }
         return max_so_far;
-        }
+    }
     
     int kConcatenationMaxSum(vector<int>& arr, int k) {
-	    
-        vector<int> res;
-        int i = 0;
+        
         long long kad = kadane(arr);
-        long long sumAll = (accumulate(arr.cbegin(), arr.cend(), 0LL) * k);
+        long long sumOfArray = accumulate(arr.cbegin(), arr.cend(), 0LL); 
+
+        long long ans2 = max<long long>(kad, sumOfArray);
+        if (k == 1)
+            return ans2;
+        
         long long prefixMax = 0;
         long long suffixMax = 0;
-        long long curr = 0;
-        for (decltype(arr.size()) i = 0; i < arr.size(); ++i) {
-            curr += arr[i];
-            if (curr > prefixMax)
-                prefixMax = curr;
+        long long currPref = 0;
+        long long currSuff = 0;
+        
+        for (int i = 0, j = arr.size() - 1; i < arr.size() && j >= 0; ++i, --j) {
+            currPref += arr[i];
+            currSuff += arr[j];
+            if (currSuff > suffixMax)
+                suffixMax = currSuff;
+            if (currPref > prefixMax)
+                prefixMax = currPref;
         }
-        curr = 0;
-        reverse(arr.begin(), arr.end());
-        for (decltype(arr.size()) i = 0; i < arr.size(); ++i) {
-            curr += arr[i];
-            if (curr > suffixMax)
-                suffixMax = curr;
-        }
+ 
+        long long sumAll = sumOfArray * k;
         long long ans = max<long long>(kad, sumAll);
-        if (k > 1) {
-            ans = max<long long>(ans, 
-    suffixMax  + prefixMax + (k - 2)*accumulate(arr.cbegin(), arr.cend(), 0LL));
-        } 
-       // long long kadd = kadane(arr);
-        long long sum = accumulate(arr.cbegin(), arr.cend(), 0LL);
-        long long anss = max<long long>(kad, sum);
-        if (k == 1)
-            return anss;
-        anss = max<long long>(prefixMax  + suffixMax, anss);
-        ans = max<long long>(ans, anss);
+
+        ans = max<long long>(ans, suffixMax  + prefixMax + (k - 2)*sumOfArray); 
+        ans2 = max<long long>(prefixMax  + suffixMax, ans2);
+        ans = max<long long>(ans, ans2);
         return ans < 0 ? 0 : ans % 1000000007;
-        
-        
         
     }
 };
