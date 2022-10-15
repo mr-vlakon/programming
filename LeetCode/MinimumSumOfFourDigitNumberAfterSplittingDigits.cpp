997cce9fab4f0ad44c2f56f44066536d9ca2cf4e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-10-15 10:12:19 +0300

    Create MinimumSumOfFourDigitNumberAfterSplittingDigits.cpp

---
+++ LeetCode/MinimumSumOfFourDigitNumberAfterSplittingDigits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> v;
+        while (num) {
+            v.push_back(num % 10);
+            num /= 10;
+        }
+        sort(v.begin(), v.end());
+        return v[0] * 10 + v[3] * 1 + v[1] * 10 + v[2] * 1;
+        
+    }
+};
+
+int main() {
+  
+ 
+  return 0;
+}
