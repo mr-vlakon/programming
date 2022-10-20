edfd0a4c69bbdfd72cbc8d72e4c9ab3ee46584a3
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-10-20 16:02:19 +0300

    Create DesignHashSet.cpp

---
+++ LeetCode/DesignHashSet.cpp
@@ -0,0 +1,37 @@
+class MyHashSet {
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        st.insert(key);
+    }
+    
+    void remove(int key) {
+        st.erase(key);
+    }
+    
+    bool contains(int key) {
+        auto it = st.find(key);
+        if (it == st.cend()) {
+            return false;
+        } else
+            return true;
+    }
+private:
+    unordered_set<int> st;
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+
+int main() {
+  
+  return 0;  
+}
