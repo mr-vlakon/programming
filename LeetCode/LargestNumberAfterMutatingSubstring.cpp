16266f068ca67d73f564cc323e5a25befdd6534e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-11-18 23:15:52 +0300

    Update LargestNumberAfterMutatingSubstring.cpp

--- LeetCode/LargestNumberAfterMutatingSubstring.cpp
+++ LeetCode/LargestNumberAfterMutatingSubstring.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     string maximumNumber(string num, vector<int>& change) {
-        unordered_map<int, int> um;
-        int i = 0;
-        for (const auto &e: change) {
-            um[i] = e;
-            ++i;
-        }
-        bool status = false;
         bool prev = false;
         for (auto &e: num) {
-            if ((e - '0' < um[e - '0'] && !status) || 
-                (e - '0' == um[e - '0'] && prev))
+            if ((e - '0' < change[e - '0'] ) || 
+                (e - '0' == change[e - '0'] && prev))
             {
                 prev = true;
-                e = um[e - '0'] + '0';
+                e = change[e - '0'] + '0';
             } else {
                 if (prev == true) {
-                    status = true;
+                    break;
                 }
             }
         }
