091139ab13bf654f37f58e45934ee91740f7961e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-11-24 15:31:31 +0300

    Update PrintFooBarAlternately.cpp

--- LeetCode/PrintFooBarAlternately.cpp
+++ LeetCode/PrintFooBarAlternately.cpp
@@ -1,44 +1,35 @@
 class FooBar {
 private:
     int n;
-
+    int turn = 1;
+    std::condition_variable cv;
+    mutex m;
 public:
     FooBar(int n) {
         this->n = n;
     }
 
     void foo(function<void()> printFoo) {
-        
+        std::unique_lock<std::mutex> lck(m);
         for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                m.lock();
-                cnt = 1;
-        	// printFoo() outputs "foo". Do not change or remove this line.
-        	    printFoo();
-                m.unlock();
-            } else {
-                --i;
-            }
+            while (turn != 1) cv.wait(lck);
+        	turn = 2;
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            cv.notify_one();
         }
     }
 
     void bar(function<void()> printBar) {
-        
+        std::unique_lock<std::mutex> lck(m);
         for (int i = 0; i < n; i++) {
-            if (cnt == 1) {
-                m.lock();
-                cnt = 0;
-        	// printBar() outputs "bar". Do not change or remove this line.
-        	    printBar();
-                m.unlock(); 
-            } else {
-                --i;
-            }
+            while (turn != 2) cv.wait(lck);
+            turn = 1;
+            // printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            cv.notify_one();
         }
     }
-private:
-    int cnt = 0;
-    mutex m;
 };
 
 int main() {
