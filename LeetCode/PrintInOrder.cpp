f9d2266bef20afd70f5fa0622bb1350d6e129c87
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-11-24 15:24:19 +0300

    Update PrintInOrder.cpp

--- LeetCode/PrintInOrder.cpp
+++ LeetCode/PrintInOrder.cpp
@@ -3,41 +3,30 @@ public:
     Foo() {
     }
     void first(function<void()> printFirst) {
-        if (tmp == 0) {
-            m.lock();
-            tmp = 1;
-            printFirst();
-            m.unlock();
-        } else {
-            first(printFirst);
-        }
-        
+        std::unique_lock<std::mutex> lck(m);
+        while (turn != 1) cv.wait(lck);
+        turn = 2;
+        printFirst();
+        cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
-            if(tmp == 1) {
-                m.lock();
-                tmp = 2;
-                printSecond();
-                m.unlock();
-            } else {
-                second(printSecond);
-            }            
+        std::unique_lock<std::mutex> lck(m);
+        while (turn != 2) cv.wait(lck);
+        turn = 3;
+        printSecond();
+        cv.notify_all();
     }
 
     void third(function<void()> printThird) {
-            if(tmp == 2) {
-                m.lock();
-                tmp = 0;
-                printThird();
-                m.unlock();
-            } else {
-                third(printThird);
-            }
+        std::unique_lock<std::mutex> lck(m);
+        while (turn != 3) cv.wait(lck);
+        printThird();
     }
 private:
-    int tmp = 0;
-    std::mutex m;
+    int turn = 1;
+    std::condition_variable cv;
+    mutex m;
 };
 
   
