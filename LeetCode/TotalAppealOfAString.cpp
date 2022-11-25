c8455bd085fb7c209f073ccb7c6f5fe081129b76
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-11-25 10:08:48 +0300

    Create TotalAppealOfAString.cpp

---
+++ LeetCode/TotalAppealOfAString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+        long long answer = 0;
+        unordered_map<char, size_t> um;
+        vector<long long> dp(s.size(), 0);
+        um[s[0]] = 1;
+        dp[0] = 1;
+        long long count = 1;
+        for (int i = 1; i != dp.size(); ++i) {
+            count -= um[s[i]];
+            um[s[i]] = i + 1;
+            count += um[s[i]];
+            dp[i] += dp[i - 1] + count;
+        }
+        return dp[dp.size() - 1];        
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
