b40c0f1ab00d3483a07a2ab4cf237acb726bfa3c
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2022-11-27 19:46:43 +0300

    Update MaximumSumOfDistinctSubarraysWithLengthK.cpp

--- LeetCode/MaximumSumOfDistinctSubarraysWithLengthK.cpp
+++ LeetCode/MaximumSumOfDistinctSubarraysWithLengthK.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         auto sum = accumulate(nums.cbegin(), nums.cbegin() + k, 0LL);
-        unordered_set<int> st(nums.cbegin(), nums.cbegin() + k);
-        vector<long long> answer;
-        if (st.size() == k) {
-            answer.push_back(sum);
+        unordered_map<int, int> um;
+        for (int i = 0; i != k; ++i) {
+            um[nums[i]]++;
         }
-        
+        long long max = 0;
+        if (um.size() == k) {
+            if (sum > max)
+                max = sum;
+        }
         for (int i = 1; i <= nums.size() - k; ++i) {
             sum -= nums[i - 1];
             sum += nums[i + k - 1];
-            st.erase(nums[i - 1]);
-            st.insert(nums[i + k - 1]);
-            if (st.size() < k) {
-                st.insert(nums[i]);
+            --um[nums[i - 1]];
+            if (um[nums[i - 1]] == 0) {
+                um.erase(nums[i - 1]);
             }
-            if (st.size() == k) {
-                answer.push_back(sum);
+            ++um[nums[i + k - 1]];
+            if (um.size() == k) {
+                if (sum > max)
+                    max = sum;
             }
-            
         }
-        if (answer.size() != 0)
-            return *max_element(answer.cbegin(), answer.cend());
-        else
-            return 0;
+        return max;
     }
 };
 
