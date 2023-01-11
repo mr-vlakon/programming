79c45fd52c89c38b0f8b81e2534ebd4e2e6818cc
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-01-11 16:54:17 +0300

    Create ExcelSheetColumnNumber.cpp

---
+++ LeetCode/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int answer = 0;
+        long long cnt = 1;
+        for (int i = columnTitle.size() - 1;  i >= 0; --i) {
+            answer += (columnTitle[i] - 'A' + 1) * cnt;
+            cnt *= 26;
+        }
+        return answer;
+    }
+};
+
+int main() {
+  
+  
+  return 0;  
+}
