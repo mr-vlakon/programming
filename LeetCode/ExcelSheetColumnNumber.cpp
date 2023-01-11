38d6735a1fa70531a6c866efe6dd6c894bb80e2b
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-01-11 16:56:02 +0300

    Update ExcelSheetColumnNumber.cpp

--- LeetCode/ExcelSheetColumnNumber.cpp
+++ LeetCode/ExcelSheetColumnNumber.cpp
@@ -4,7 +4,7 @@ public:
         int answer = 0;
         long long cnt = 1;
         for (int i = columnTitle.size() - 1;  i >= 0; --i) {
-            answer += (columnTitle[i] - 'A' + 1) * cnt;
+            answer += (columnTitle[i] - 'A' + 1) * static_cast<int>(cnt);
             cnt *= 26;
         }
         return answer;
