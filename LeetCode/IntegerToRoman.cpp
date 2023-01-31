e470af07f3f457ca8bfc0b6104f822834629a36c
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-01-31 23:50:26 +0300

    Update IntegerToRoman.cpp

--- LeetCode/IntegerToRoman.cpp
+++ LeetCode/IntegerToRoman.cpp
@@ -10,67 +10,65 @@ public:
                 continue;
             }
             num -= tmp;
-            if (cnt == 10 && tmp == 9) {
+            if (tmp == 9) {
                 result.push_back('X');
                 result.push_back('I');
-            } else if (cnt == 10 && tmp >= 5) {
+            } else if (tmp >= 5 && tmp < 9) {
                 int diff = tmp - 5;
                 while (diff) {
                     result.push_back('I');
                     --diff;
                 }
                 result.push_back('V');    
-            } else if (cnt == 10 && tmp == 4) {
+            } else if (tmp == 4) {
                     result.push_back('V');
                     result.push_back('I');
-            } else if (cnt == 10 && tmp < 4) {
+            } else if (tmp < 4) {
                 while (tmp) {
                     result.push_back('I');
                     --tmp;
                 }
-            } else if (cnt == 100 && tmp == 50) {
-                result.push_back('L');
-            } else if (cnt == 100 && tmp == 10) {
+            } else if (tmp == 10) {
                 result.push_back('X');
-            } else if (cnt == 100 && tmp == 40) {
+            } else if (tmp == 40) {
                 result.push_back('L');
                 result.push_back('X');
-            } else if (cnt == 100 && tmp == 90) {
+            } else if (tmp == 90) {
                 result.push_back('C');
                 result.push_back('X');
-            } else if (cnt == 100 && tmp >= 50){
+            } else if (tmp >= 50 && tmp < 100){
                 int diff = tmp - 50;
                 while (diff) {
                     result.push_back('X');
                     diff -= 10;
                 }
                 result.push_back('L');
-            } else if (cnt == 100 && tmp < 50) {
+            } else if (tmp < 50) {
                 while (tmp > 0) {
                     result.push_back('X');
                     tmp -= 10;
                 }
-            } else if (cnt == 1000 && tmp == 500) {
+            } else if (tmp == 500) {
                 result.push_back('D');
-            } else if (cnt == 1000 && tmp == 400) {
+            } else if (tmp == 400) {
                 result.push_back('D');
                 result.push_back('C'); 
-            } else if (cnt == 1000 && tmp < 400) {
+            } else if (tmp < 400) {
                 while (tmp > 0) {
                     result.push_back('C');
                     tmp -= 100;
                 }
-            } else if (cnt == 1000 && tmp == 900) {
+            } else if (tmp == 900) {
                 result.push_back('M');
                 result.push_back('C');
-            } else if (cnt == 1000 && tmp > 500) {
+            } else if (tmp < 900) {
                 int diff = tmp - 500;
                 while (diff > 0) {
                     result.push_back('C');
                     diff -= 100;
                 }
                 result.push_back('D');
-            } else if (cnt == 10000 && tmp >= 1000) {
+            } else if (tmp >= 1000) {
                 while (tmp) {
                     result.push_back('M');
                     tmp -= 1000;
@@ -78,8 +76,7 @@ public:
             }
             cnt *= 10;
         }
-        reverse(result.begin(), result.end());
-        return result;
+        return {result.rbegin(), result.rend()};
     }
 };
 
