bd8c44c3b7d552ee3ee8c26f41de19ee2e5dd292
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-01 21:37:05 +0300

    Update TwoCityScheduling.cpp

--- LeetCode/TwoCityScheduling.cpp
+++ LeetCode/TwoCityScheduling.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        vector<pair<int, vector<int>> > v;
+        vector<pair<int, int>> v;
         for (const auto &e: costs) {
-            v.push_back({e[0] - e[1], {e[0], e[1]}});
+            v.push_back({e[0] - e[1], e[0]});
         }
-        sort(v.begin(), v.end(), [] (pair<int, vector<int>> &lhs, pair<int, vector<int>> &rhs) {
+        sort(v.begin(), v.end(), [] (pair<int, int> &lhs, pair<int, int> &rhs) {
             return lhs.first < rhs.first;
         });
         int cnt = 0;
         int answer = 0;
         for (int i = 0; i != v.size(); i++) {
             if (cnt < (v.size() / 2)) {
-                answer += v[i].second[0];
+                answer += v[i].second;
             } else {
-                answer += v[i].second[1];
+                answer += v[i].second - v[i].first;
             }
             ++cnt;
         }
