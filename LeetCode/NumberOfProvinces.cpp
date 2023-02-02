d9884e13fdb6868cda9c2b2c4ea2d2aad5adb139
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-02 13:58:09 +0300

    Create NumberOfProvinces.cpp

---
+++ LeetCode/NumberOfProvinces.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int, unordered_set<int>> um;
+        int i = 1;
+        for (const auto &e: isConnected) {
+            //um[i];
+            int j = 1;
+            for (const auto &p: e) {
+                if (p == 1 && i != j) {
+                    um[i].insert(j);
+                }
+                ++j;
+            }
+            ++i;
+        }
+        i = 1;
+        queue<int> q;
+        int cnt = 0;
+        unordered_set<int> us;
+        while (i <= isConnected.size()) {
+            if (us.find(i) != us.cend()) {
+                ++i;
+                continue;
+            }
+            ++cnt;
+            q.push(i);
+            us.insert(i);
+            while(!q.empty()) {
+                auto top = q.front();
+                q.pop();
+                for (const auto &e: um[top]) {
+                    if ((us.find(e) == us.cend())) {    
+                        us.insert(e);
+                        q.push(e);
+                    }
+                }   
+            }
+            ++i;
+        }
+        return cnt; 
+    }
+};
+
+int main() {
+  
+  
+  return 0;  
+}
