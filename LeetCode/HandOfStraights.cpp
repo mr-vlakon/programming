a0ecb33b0082ea9b187e36f5cecf6bf5d1779d1e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-21 15:49:17 +0300

    Create HandOfStraights.cpp

---
+++ LeetCode/HandOfStraights.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int k = groupSize;
+        unordered_map<int, int> um;
+        for (const auto &e: hand) {
+            um[e]++;
+        }
+        vector<pair<int, int>> v;
+        for (const auto &e: um) {
+            v.push_back({e.first, e.second});
+        }
+        sort (v.begin(), v.end(), [](pair<int, int> &lhs, pair<int, int> &rhs) {
+            return lhs.first < rhs.first;
+        });
+        int start = 0;
+        int cnt = 0;
+        int prev = -1;
+        bool status = false;    
+        while (true) {
+            cnt = 0;
+            prev = -1;
+            status = false;
+            for (int i = start; i < v.size(); ++i) {
+                if (cnt == k) {
+                    break;
+                }
+                if ((v[i].second != 0) && (prev == -1 || ((prev + 1) == v[i].first))) {
+                    ++cnt;
+                    --v[i].second;
+                    if ((v[i].second != 0 ) && !status) {
+                        start = i;
+                        status = true;
+                    }
+                } else if (prev != (v[i].first + 1)) {
+                    return false;
+                }
+                prev = v[i].first;
+            }
+            if (cnt != k) {
+                return false;
+            }
+            if (!status && (start + k) < v.size()) {
+                start = start + k;
+            } else if (!status) {
+                break;
+            }
+        }
+        return true;      
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
