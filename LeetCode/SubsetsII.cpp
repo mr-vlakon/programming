205115c420dc4deb3501a779ffed452f285cf4d4
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-25 18:09:19 +0300

    Update SubsetsII.cpp

--- LeetCode/SubsetsII.cpp
+++ LeetCode/SubsetsII.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void generate(int index = -1, vector<int> curr = {}) {
@@ -26,6 +27,32 @@ public:
 private:
     vector<int> v;
     vector<vector<int>> res;
+};
+*/
+
+class Solution {
+public:
+    void generate(const vector<int> &num, int i, vector<int> &v) {
+        if (i == num.size()) {
+            result.push_back(v);
+            return;
+        }
+        v.push_back(num[i]);
+        generate(num, i + 1, v);
+        v.pop_back();
+        generate(num, i + 1, v);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> empty;
+        sort(nums.begin(), nums.end());
+        generate(nums, 0, empty);
+        sort(result.begin(), result.end());
+        auto p = unique(result.begin(), result.end());
+        //result.erase(p, result.end());
+        return {result.begin(), p};
+    }
+private:
+    vector<vector<int>> result;
 };
 
 int main() {
