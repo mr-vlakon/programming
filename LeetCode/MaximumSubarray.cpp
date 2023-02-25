1df4433bdf1f6c73de18817f42bacf6f278b1007
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-25 15:19:08 +0300

    Update MaximumSubarray.cpp

--- LeetCode/MaximumSubarray.cpp
+++ LeetCode/MaximumSubarray.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int maxSubArraySum(const vector<int> &v) {
-       int max_so_far = v[0];
-       int curr_max = v[0];
-       for (decltype(v.size()) i = 1; i < v.size(); ++i) {
-            curr_max = max(v[i], curr_max + v[i]);
-            max_so_far = max(max_so_far, curr_max);
-       }
-       return max_so_far;
-    }
-    
     int maxSubArray(vector<int>& nums) {
-        return maxSubArraySum(nums);    
+        int currSum = 0;
+        int answer = -100000;
+        for (const auto &e: nums) {
+            currSum += e;
+            if (currSum > answer) {
+                answer = currSum;
+            }
+            currSum = (currSum < 0) ? 0 : currSum;
+        }
+        return answer;
     }
 };
 
