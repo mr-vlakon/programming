bd448bb0c84c6f36a87c81f3ed35603950a06c4d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-25 12:16:49 +0300

    Create FlipEquivalentBinaryTrees.cpp

---
+++ LeetCode/FlipEquivalentBinaryTrees.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* &p, TreeNode* &q) {
+        if (p == nullptr) return;
+        if (q == nullptr) return;
+        if (p->left && q->left && p->left->val != q->left->val) {
+            swap(p->left, p->right);
+        }
+        if (p->left == nullptr && q->left) {
+            swap(p->left, p->right);
+        }
+        if (p->right == nullptr && q->right) {
+            swap(p->left, p->right);
+        }
+        traverse(p->left, q->left);
+        traverse(p->right, q->right);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (q == nullptr || p == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        traverse(root1, root2);
+        return isSameTree(root1, root2);
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
