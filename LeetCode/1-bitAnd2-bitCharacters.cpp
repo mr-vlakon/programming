60dde97676929993fde9d068b8fcbd9bcfee4841
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-28 00:09:19 +0300

    Update 1-bitAnd2-bitCharacters.cpp

--- LeetCode/1-bitAnd2-bitCharacters.cpp
+++ LeetCode/1-bitAnd2-bitCharacters.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    void generate(const vector<int> &bit, int i, int cnt) {
+    void generate(const vector<int> &bit, int i) {
         if (i == (bit.size() - 1)) {
-            if (cnt == 0) {
-                status = true;
-            }
+            status = true;
             return;
         }
         if (i >= bit.size()) {
             return;
         }
-        if (cnt == 0) {
-            if (bit[i] == 0) {
-                generate(bit, i + 1, 0);
-            } else if (bit[i] == 1) {
-                generate(bit, i + 2, 0);    
-            }
-        } else if (cnt == 1) {
-            if (bit[i] == 1) {
-                generate(bit, i + 2, 0);   
-            }
+        if (bit[i] == 0) {
+            generate(bit, i + 1);
+        } else if (bit[i] == 1) {
+            generate(bit, i + 2);    
         }
     }
     bool isOneBitCharacter(vector<int>& bits) {
-        generate(bits, 0, 0);
+        generate(bits, 0);
         return status;
     }
 private:
