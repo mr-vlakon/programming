05fc551ca47d537e0894f3ffaffc29896c4134ff
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-02-28 13:48:22 +0300

    Update CircularPermutationInBinaryRepresentation.cpp

--- LeetCode/CircularPermutationInBinaryRepresentation.cpp
+++ LeetCode/CircularPermutationInBinaryRepresentation.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     void generate(int &i) {
-        if (i == (n_ - 1)) {
+        if (i == n_) {
             return;
         }
-        int tmp = 0;
+        if (i > n_) {
+            return;
+        }
         for (int k = 0; k != 31; ++k) {
             tmp = result[i - 1] ^ (1 << k);
-            if (us.find(tmp) == us.cend()) {
-                us.insert(tmp);
+            if (tmp < n_ && ban[tmp] == false) {
+                ban[tmp] = true;
                 result[i] = tmp;
+                ++i;
+                generate(i);
                 break;
-            }
+            }      
         } 
-        ++i;
-        generate(i);
     }
     vector<int> circularPermutation(int n, int start) {
         n_ = pow(2, n);
-        result.resize(pow(2, n), 0);
+        result.resize(n_, 0);
+        ban.resize(n_, false);
+        ban[start] = true;
         result[0] = start;
-        us.insert(result[0]);
-        result[result.size() - 1] = result[0] ^ (1 << 0);
-        us.insert(result[result.size() - 1]);
         int i = 1;
         generate(i);
         return result;
     }
 private:
-    unordered_set<int> us;
+    vector<bool> ban;
+    int tmp = 0;
     int n_ = 0;
     vector<int> result;
 };
