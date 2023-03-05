75837164caaadb8dc44adee92c2e346a682d1160
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-05 21:52:05 +0300

    Update FindPeakElement.cpp

--- LeetCode/FindPeakElement.cpp
+++ LeetCode/FindPeakElement.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    void recursive(const vector<int> &num, int &start, int &end, int &mid) {
+        if (mid > 0 && mid < (num.size() - 1) && num[mid] > num[mid - 1] && num[mid] > num[mid + 1]) {
+            answer = mid;
+            return;
+        }
+        if (mid == 0 && num[mid] > num[mid + 1]) {
+            answer = mid;
+            return;
+        }
+        if (mid == (num.size() - 1) && num[mid] > num[mid - 1]) {
+            answer = mid;
+            return;
+        }
+        if (start >= end) {
+            return;
+        }
+        if ((mid + 1) < num.size() && num[mid] < num[mid + 1]) {
+            start = mid + 1;
+            mid = (start + end) / 2;
+            recursive(num, start, end, mid);
+        } else if ((mid - 1) >= 0 && num[mid] < num[mid - 1]) {
+            end = mid - 1;
+            mid = (start + end) / 2;
+            recursive(num, start, end, mid);
+        }
+    }
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = nums.size() / 2;
+        recursive(nums, start, end, mid);
+        return answer;
+    }
+private:
+    int answer = 0;
+};
+
+/*
+class Solution {
+public:
     int findPeakElement(vector<int>& nums) {
         if (nums.size() == 0) return 0;
         if (nums.size() == 1) return 0;
@@ -24,6 +67,7 @@ public:
         return it->second;
     }
 };
+*/
 
 int main() {
   
