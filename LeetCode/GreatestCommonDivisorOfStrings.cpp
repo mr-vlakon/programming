1cee3e52a69cd8987b04666efdd5edc5fd3a8091
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-20 00:26:33 +0300

    Update GreatestCommonDivisorOfStrings.cpp

--- LeetCode/GreatestCommonDivisorOfStrings.cpp
+++ LeetCode/GreatestCommonDivisorOfStrings.cpp
@@ -8,19 +8,22 @@ public:
             string str;
             for (const auto &e: str1) {
                 tmp.push_back(e);
-                while (tmp2.size() < str2.size()) {
-                    tmp2 += tmp;
-                }
-                if (tmp2 == str2) {
-                    str = tmp;
-                    while (str.size() < str1.size()) {
-                        str += tmp;
+                if ((str2.size() % tmp.size()) == 0 && (str1.size() % tmp.size()) == 0) {
+                    tmp2 = tmp;
+                    while (tmp2.size() < str2.size()) {
+                        tmp2 += tmp;
                     }
-                    if (str == str1) {
-                        answer = tmp;
+                    if (tmp2 == str2) {
+                        str = tmp;
+                        while (str.size() < str1.size()) {
+                            str += tmp;
+                        }
+                        if (str == str1) {
+                            answer = tmp;
+                        }
                     }
+                    tmp2.resize(0);      
                 }
-                tmp2.resize(0);      
             }
         } else {
             string tmp;
@@ -28,19 +31,22 @@ public:
             string str;
             for (const auto &e: str2) {
                 tmp.push_back(e);
-                while (tmp2.size() < str1.size()) {
-                    tmp2 += tmp;
-                }
-                if (tmp2 == str1) {
-                    str = tmp;
-                    while (str.size() < str2.size()) {
-                        str += tmp;
+                if ((str1.size() % tmp.size()) == 0 && (str2.size() % tmp.size()) == 0) {
+                    tmp2 = tmp;
+                    while (tmp2.size() < str1.size()) {
+                        tmp2 += tmp;
                     }
-                    if (str == str2) {
-                        answer = tmp;
+                    if (tmp2 == str1) {
+                        str = tmp;
+                        while (str.size() < str2.size()) {
+                            str += tmp;
+                        }
+                        if (str == str2) {
+                            answer = tmp;
+                        }
                     }
+                    tmp2.resize(0);      
                 }
-                tmp2.resize(0);      
             }
         }
         return answer;
