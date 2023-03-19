e76c460116eb6d3fd97275e12e815cbe629aebc2
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-19 23:47:47 +0300

    Create GreatestCommonDivisorOfStrings.cpp

---
+++ LeetCode/GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string answer;
+        if (str1.size() < str2.size()) {
+            string tmp;
+            string tmp2;
+            string str;
+            for (const auto &e: str1) {
+                tmp.push_back(e);
+                while (tmp2.size() < str2.size()) {
+                    tmp2 += tmp;
+                }
+                if (tmp2 == str2) {
+                    str = tmp;
+                    while (str.size() < str1.size()) {
+                        str += tmp;
+                    }
+                    if (str == str1) {
+                        answer = tmp;
+                    }
+                }
+                tmp2.resize(0);      
+            }
+        } else {
+            string tmp;
+            string tmp2;
+            string str;
+            for (const auto &e: str2) {
+                tmp.push_back(e);
+                while (tmp2.size() < str1.size()) {
+                    tmp2 += tmp;
+                }
+                if (tmp2 == str1) {
+                    str = tmp;
+                    while (str.size() < str2.size()) {
+                        str += tmp;
+                    }
+                    if (str == str2) {
+                        answer = tmp;
+                    }
+                }
+                tmp2.resize(0);      
+            }
+        }
+        return answer;
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
