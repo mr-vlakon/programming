61e5b4d02daf6de495f5ce7534ce6bbc76a7418b
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-24 00:29:32 +0300

    Update H-Index.cpp

--- LeetCode/H-Index.cpp
+++ LeetCode/H-Index.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
@@ -12,6 +13,36 @@ public:
         }
         return answer;
     }
+};
+*/
+class Solution {
+public:
+    void binary(const vector<int> &cit, int left, int right) {
+        if (left < 0) return;
+        if (right > cit.size()) return;
+        if (left > right) return;
+        mid = (left + right) / 2;
+        if (cit[mid] >= (mid + 1)) {
+            ans = max(ans, mid + 1);
+            if (left != right) {
+                binary(cit, mid + 1, right);
+            }
+            return;
+        } else {
+            if (left != right) {
+                binary(cit, left, mid - 1);
+            }
+            return;
+        }
+    }
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(), citations.rend());
+        binary(citations, 0, citations.size() - 1);
+        return ans;
+    }
+private:
+    int mid = 0;
+    int ans = 0;
 };
 
 int main() {
