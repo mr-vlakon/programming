0d270467c00e1611bbc68d4662b0afc9ba6d29db
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-24 14:20:52 +0300

    Update KokoEatingBananas.cpp

--- LeetCode/KokoEatingBananas.cpp
+++ LeetCode/KokoEatingBananas.cpp
@@ -4,44 +4,33 @@ public:
         if (left > right) {
             return;
         }
-        if (left < 0) {
-            return;
-        }
         tmp = 0;
         mid = (left + right) / 2;
-        if (mid == 0) return;
         for (const auto &e: nums) {
-            tmp += ceil(static_cast<double>(e) / static_cast<double>(mid));
+            tmp += ceil(static_cast<double>(e) / static_cast<double>(mid + 1));
         }
         if (tmp <= th) {
-            answer = min(answer, mid);
+            answer = min(answer, mid + 1);
             right = mid - 1;
             binary(nums, left, right); 
         } else {
-            left = mid + 1;
+            left = mid  + 1; 
             binary(nums, left, right);        
         }
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
         th = h;
-        int maxV = *max_element(piles.cbegin(), piles.cend());
+        int maxV = *max_element(piles.cbegin(), piles.cend()) - 1;
         int left = 0;
         binary(piles, left, maxV);
-        mid = 1;
-        tmp = 0;
-        for (const auto &e: piles) {
-            tmp += e;
-        }
-        if (tmp <= th) {
-            answer = min(answer, 1);
-        }
         return answer;
     }
 private:
     int mid = 0;
     int th = 0;
     long long tmp = 0;
-    int answer = 10000000000;
+    int answer = 1000000001;
 };
 
 int main() {
