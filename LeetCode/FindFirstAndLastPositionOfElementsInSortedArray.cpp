dac91412f5e057d25ed9389da18c4a7485b1a6a2
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-03-29 11:47:49 +0300

    Update FindFirstAndLastPositionOfElementsInSortedArray.cpp

--- LeetCode/FindFirstAndLastPositionOfElementsInSortedArray.cpp
+++ LeetCode/FindFirstAndLastPositionOfElementsInSortedArray.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
@@ -9,6 +10,58 @@ public:
         else
             return {indx1, --indx2};
     }
+};
+*/
+class Solution {
+public:
+    void binaryLeft(const vector<int> &nums, const int &tar, int &left, int &right) {
+        if (left > right) {
+            return;
+        }
+        mid = (left + right) / 2;
+        if (nums[mid] > tar) {
+            right = mid - 1;
+            binaryLeft(nums, tar, left, right);
+        } else if (nums[mid] < tar){
+            left = mid + 1;
+            binaryLeft(nums, tar, left, right);
+        } else if (nums[mid] == tar) {
+            start = mid;
+            right = mid - 1;
+            binaryLeft(nums, tar, left, right);
+        }
+    }
+    void binaryRight(const vector<int> &nums, const int &tar, int &left, int &right) {
+        if (left > right) {
+            return;
+        }
+        mid = (left + right) / 2;
+        if (nums[mid] > tar) {
+            right = mid - 1;
+            binaryRight(nums, tar, left, right);
+        } else if (nums[mid] < tar){
+            left = mid + 1;
+            binaryRight(nums, tar, left, right);
+        } else if (nums[mid] == tar) {
+            end = mid;
+            left = mid + 1;
+            binaryRight(nums, tar, left, right);
+        }
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        binaryLeft(nums, target, left, right);
+        left = 0;
+        right = nums.size() - 1;
+        binaryRight(nums, target, left, right);
+        return {start, end};
+    }
+private:
+    int end = -1;        
+    int start = -1;
+    int mid = 0;
 };
 
 int main() {
