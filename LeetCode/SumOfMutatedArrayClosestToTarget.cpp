7a03cf13a238e43a449e6cf26b312d847d054583
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-07 17:26:05 +0300

    Update SumOfMutatedArrayClosestToTarget.cpp

--- LeetCode/SumOfMutatedArrayClosestToTarget.cpp
+++ LeetCode/SumOfMutatedArrayClosestToTarget.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void binary(const vector<int> &arr, const int &target, int &left, int &right) {
@@ -35,6 +36,49 @@ private:
     int mid = 0;
     int diff = 0;
     int answer = 0;
+};
+*/
+class Solution {
+public:
+    void binary(const vector<int> &arr, const int &target, int &left, int &right) {
+        if (left > right) return;
+        mid = (left + right) / 2;
+        sum = 0;
+        for (int i = 0; i != arr.size(); ++i) {
+            if (arr[i] > mid) {
+                sum += mid;
+            } else {
+                sum += arr[i];
+            }
+        }
+        if (abs(sum - target) <= diff) {
+            if (diff == abs(sum - target)) {
+                answer = min(answer, mid);
+            } else {
+                answer = mid;
+            }
+            diff = abs(sum - target);
+        }
+        if (sum > target) {
+            right = mid - 1;
+            binary(arr, target, left, right);
+        } else {
+            left = mid + 1;
+            binary(arr, target, left, right);
+        }
+    }
+    int findBestValue(vector<int>& arr, int target) {
+        int left = 0;
+        int right = *max_element(arr.cbegin(), arr.cend());
+        diff = 100000000;
+        binary(arr, target, left, right);
+        return answer;
+    }
+private:
+    int sum = 0;
+    int mid = 0;
+    int diff = 0;
+    int answer = 1000000000;
 };
 
 int main() {
