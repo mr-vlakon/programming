2df36f59fceb001e3512ab35086b0d7b0be6a529
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-08 23:52:02 +0300

    Update BestSightseeingPair.cpp

--- LeetCode/BestSightseeingPair.cpp
+++ LeetCode/BestSightseeingPair.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        vector<pair<int, int>> dp(values.size(), {0, 0});
-        dp[0].first = 0;
-        dp[0].second = values[0];
-        dp[1].first = values[0] + 0 + values[1] - 1; 
-        dp[1].second = max(dp[0].second, values[1] + 1);
+        int maxV = max(values[0], values[1] + 1);
+        int maxF = values[0] + values[1] - 1;
         for (int i = 2; i != values.size(); ++i) {
-            dp[i].first = max(dp[i - 1].first, values[i] - i + dp[i - 1].second);
-            dp[i].second = max(dp[i - 1].second, values[i] + i);
+            maxF = max(maxF, values[i] - i + maxV);
+            maxV = max(maxV, values[i] + i);
         }
-        return dp.back().first;
+        return maxF;
     }
 };
 
