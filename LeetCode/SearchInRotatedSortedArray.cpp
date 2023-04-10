eff316315aff6401caa86af46988e34d3c75ec10
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-10 23:08:26 +0300

    Update SearchInRotatedSortedArray.cpp

--- LeetCode/SearchInRotatedSortedArray.cpp
+++ LeetCode/SearchInRotatedSortedArray.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        map<int,size_t> mp;
-        auto index = 0;
-        for (const auto &e: nums) {
-            mp[e] = index++;
+    void binary(const vector<int> &nums, const int &target, int &left, int &right) {
+        if (left > right) return;
+        mid = (left + right) / 2;
+        if (nums[mid] == target) {
+            answer = mid;
+        } else if (nums[mid] < nums[right] && target <= nums[right] && target > nums[mid]) {
+            left = mid + 1;
+            binary(nums, target, left, right);
+        } else if (nums[mid] > nums[left] && target >= nums[left] && target < nums[mid]) {
+            right = mid - 1;
+            binary(nums, target, left, right);
+        } else if (nums[right] < nums[mid] && (target > nums[mid] || target <= nums[right])) {
+            left = mid + 1;
+            binary(nums, target, left, right);
+        } else  {
+            right = mid - 1;
+            binary(nums, target, left, right);
         }
-        
-        if (mp.find(target) == mp.cend()) {
-            return -1;
-        } else
-            return mp[target];
     }
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        binary(nums, target, left, right);
+        return answer;
+    }
+private:
+    int answer = -1;
+    int mid = 0;
 };
 
 int main() {
