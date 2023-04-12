cff921771255ec51e9fa5014cb1fe8b69b5ccd30
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-12 15:14:16 +0300

    Update AlertUsingSameKey-CardThreeOrMoreTimesInOneHourPeriod.cpp

--- LeetCode/AlertUsingSameKey-CardThreeOrMoreTimesInOneHourPeriod.cpp
+++ LeetCode/AlertUsingSameKey-CardThreeOrMoreTimesInOneHourPeriod.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int convertToInt(const string &s) {
-        return stoi(string{s.cbegin(), s.cbegin() + 2})*60 + stoi(string{s.cbegin() + 3, s.cend()});
+        return 10*(s[0] - '0')*60 + (s[1] - '0')*60 + (s[3] - '0')*10 + (s[4] - '0');
     }
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
-        vector<pair<string, int>> v;
-        for (int i = 0; i != keyName.size(); ++i) {
-            v.push_back({keyName[i], convertToInt(keyTime[i])});
-        }
-        sort(v.begin(), v.end(), [](pair<string, int> &lhs, pair<string, int> &rhs) {
-            return lhs.second < rhs.second;
-        });
         unordered_map<string, vector<int>> um;
-        for (const auto &e: v) {
-            um[e.first].push_back(e.second);
+        for (int i = 0; i != keyName.size(); ++i) {
+            um[keyName[i]].push_back(convertToInt(keyTime[i]));
+        }
+        for (auto &e: um) {
+            sort(e.second.begin(), e.second.end());
         }
         vector<string> answer;
         for (const auto &e: um) {
