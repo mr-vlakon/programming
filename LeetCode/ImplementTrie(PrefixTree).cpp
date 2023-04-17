1641d6429a24d0195f6550ac914f2600bae41675
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-17 15:48:19 +0300

    Update ImplementTrie(PrefixTree).cpp

--- LeetCode/ImplementTrie(PrefixTree).cpp
+++ LeetCode/ImplementTrie(PrefixTree).cpp
@@ -1,3 +1,4 @@
+/*
 class Trie {
 public:
     Trie() {
@@ -34,7 +35,45 @@ private:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+*/
+class Trie {
+public:
+    Trie() {
+        
+    }
+    void construct(string &word) {
+        if (word.size() == 0) return;
+        if (us.find(word) != us.cend()) return;
+        else us.insert(word);
+        word.pop_back();
+        construct(word);
+    }
 
+    void insert(string word) {
+        searchable.insert(word);
+        if (us.find(word) != us.cend()) return;
+        construct(word);
+    }
+    
+    bool search(string word) {
+        return searchable.find(word) != searchable.cend();
+    }
+    
+    bool startsWith(string prefix) {
+        return us.find(prefix) != us.cend();
+    }
+private:
+    unordered_set<string> searchable;
+    unordered_set<string> us;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
 int main() {
   
     
