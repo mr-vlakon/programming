3b266aa65d9e3e854e64bde2c002e3f933ff263d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-18 10:44:38 +0300

    Create ArrayOfDoubledPairs.cpp

---
+++ LeetCode/ArrayOfDoubledPairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        unordered_map<int, int> um;
+        for (const auto &e: arr) {
+            ++um[e];
+        }
+        unordered_map<int, int>::iterator it1;
+        unordered_map<int, int>::iterator it2;
+        for (const auto &e: arr) {
+            it1 = um.find(e * 2);
+            it2 = um.find(e);
+            if (it1 != um.cend() && it2 != um.cend()) {
+                um[e * 2]--;
+                um[e]--;
+                if (um[e * 2] == 0) {
+                    um.erase(e * 2);
+                }
+                if (um[e] == 0) {
+                    um.erase(e);
+                }
+            }
+        }
+        return um.size() == 0;
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
