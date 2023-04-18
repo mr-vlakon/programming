89e4bdb8c6217c1ea0ae02c297b22dc2ff71fb1c
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-18 20:33:58 +0300

    Update PeekingIterator.cpp

--- LeetCode/PeekingIterator.cpp
+++ LeetCode/PeekingIterator.cpp
@@ -16,7 +16,7 @@
  *		bool hasNext() const;
  *	};
  */
-
+/*
 class PeekingIterator : public Iterator {
 public:
 	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
@@ -39,6 +39,43 @@ public:
 	bool hasNext() const {
 		return this->Iterator::hasNext();
 	}
+};
+*/
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+		status = this->Iterator::hasNext();
+    if (status) {
+      valN = this->Iterator::next();
+    }
+        	// Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+		return valN;
+ 	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+		val = valN;
+    status = this->Iterator::hasNext();
+    if (status) {
+      valN = this->Iterator::next();
+    }
+    return val;
+	}
+	
+	bool hasNext() const {
+		return status;
+	}
+private:
+    int val = 0;
+    bool status = false;
+    int valN = 0;
 };
 
 int main() {
