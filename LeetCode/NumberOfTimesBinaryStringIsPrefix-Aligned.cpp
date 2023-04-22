6a460e29286abe83750cf0c6780032cfeead54c7
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-22 14:08:28 +0300

    Create NumberOfTimesBinaryStringIsPrefix-Aligned.cpp

---
+++ LeetCode/NumberOfTimesBinaryStringIsPrefix-Aligned.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numTimesAllBlue(vector<int>& flips) {
+        int answer = 0;
+        int last1 = -1;
+        for (int i = 0; i != flips.size(); ++i) {
+            if (flips[i] > last1) {
+                last1 = flips[i];
+            }
+            if (last1 == (i + 1)) {
+                ++answer;
+            }
+        }
+        return answer;
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
