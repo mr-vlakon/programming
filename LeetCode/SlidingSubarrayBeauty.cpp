4193c18988df6a0833d47756c876fe1401ec9a50
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-23 07:26:12 +0300

    Update SlidingSubarrayBeauty.cpp

--- LeetCode/SlidingSubarrayBeauty.cpp
+++ LeetCode/SlidingSubarrayBeauty.cpp
@@ -31,6 +31,62 @@ public:
     }
 };
 
+/*
+Other solution
+
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        vector<int> v(50, 0);
+        for (int i = 0; i != k; ++i) {
+            if (nums[i] < 0) {
+                v[abs(nums[i]) - 1]++;
+            }
+        }
+        vector<int> answer;
+        int cnt = 0;
+        int find = 0;
+        for (int i = v.size() - 1; i >= 0; --i) {
+            if (cnt < x) {
+                cnt += v[i];
+                find = -(i + 1);
+            } else {
+                break;
+            }
+        }
+        if (cnt < x) {
+            answer.push_back(0);
+        } else {
+            answer.push_back(find);
+        }
+        for (int i = 1; i < nums.size() - k + 1; ++i) {
+            cnt = 0;            
+            find = 0;
+            if (nums[i - 1] < 0) {
+                v[abs(nums[i - 1]) - 1]--;
+            }
+            if (nums[i + k - 1] < 0) {
+                v[abs(nums[i + k - 1]) - 1]++;
+            }
+            for (int i = (v.size() - 1); i >= 0; --i) {
+                if (cnt < x) {
+                    cnt += v[i];
+                    find = -(i + 1);
+                } else {
+                    break;
+                }
+            }
+            if (cnt < x) {
+                answer.push_back(0);
+            } else
+                answer.push_back(find);
+        }    
+        return answer;
+    }
+};
+
+*/
+
 int main() {
   
   
