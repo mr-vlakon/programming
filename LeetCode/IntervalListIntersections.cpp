67cffbc62b2fb37e2c7a016c858e48155693f355
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-24 13:55:00 +0300

    Update IntervalListIntersections.cpp

--- LeetCode/IntervalListIntersections.cpp
+++ LeetCode/IntervalListIntersections.cpp
@@ -12,6 +12,31 @@ public:
                 }
                 first++;
             } else {
+                if (secondList[second][1] >= firstList[first][0]) {
+                    answer.push_back({max(firstList[first][0], secondList[second][0]), 
+                    secondList[second][1]});
+                }
+                ++second;
+            }
+        }
+        return answer;
+  }
+};
+/*
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> answer;
+        int first = 0;
+        int second = 0;
+        while ((first != firstList.size() && second != secondList.size())) {
+            if (firstList[first][1] <= secondList[second][1]) {
+                if (firstList[first][1] >= secondList[second][0]) {
+                    answer.push_back({max(firstList[first][0], secondList[second][0]), 
+                    firstList[first][1]});
+                }
+                first++;
+            } else {
                 if ((secondList[second][0] >= firstList[first][0]) || (secondList[second][1] >= firstList[first][0])) {
                     answer.push_back({max(firstList[first][0], secondList[second][0]), min(firstList[first][1], secondList[second][1])});
                 }
@@ -21,6 +46,7 @@ public:
         return answer;
   }
 };
+*/
 
 int main() {
   
