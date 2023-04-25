df399e58bc98b4956ebf4654ea2ae245db22a3a2
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-25 12:51:14 +0300

    Update MinimumTimeToMakeRopeColorful.cpp

--- LeetCode/MinimumTimeToMakeRopeColorful.cpp
+++ LeetCode/MinimumTimeToMakeRopeColorful.cpp
@@ -4,6 +4,31 @@ public:
         int answer = 0;
         int maxV = 0;
         int tmp = 0;
+        tmp = neededTime[0];
+        maxV = neededTime[0];
+        for (int i = 1; i != colors.size(); ++i) {
+            if (colors[i] == colors[i - 1]) {
+                tmp += neededTime[i];
+                maxV = max(maxV, neededTime[i]);
+            } else {
+                tmp -= maxV;
+                maxV = neededTime[i];
+                answer += tmp;
+                tmp = neededTime[i];
+            }
+        }
+        tmp -= maxV;
+        answer += tmp;
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int answer = 0;
+        int maxV = 0;
+        int tmp = 0;
         char prev;
         for (int i = 0; i != colors.size(); ++i) {
             if (colors[i] == prev) {
@@ -22,6 +47,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
   
