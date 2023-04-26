5e43e474a6dc4abbee1291c16646565055bef6f4
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-26 14:38:19 +0300

    Update MinimizeTheMaximumDifferenceOfPairs.cpp

--- LeetCode/MinimizeTheMaximumDifferenceOfPairs.cpp
+++ LeetCode/MinimizeTheMaximumDifferenceOfPairs.cpp
@@ -4,6 +4,47 @@ public:
         if (left > right) return;
         mid = (left + right) / 2;
         cnt = 0;
+        taken = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            if (abs(nums[i] - taken) <= mid) {
+                ++cnt;
+                if ((i + 1) < nums.size())
+                    taken = nums[i + 1];
+                ++i;
+            } else {
+                taken = nums[i];
+            }
+        } 
+        if (cnt < p) {
+            left = mid + 1;
+            binary(nums, p, left, right);
+        } else {
+            answer = min(answer, mid);
+            right = mid - 1;
+            binary(nums, p, left, right);
+        }
+    }
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = nums.back() - nums[0];
+        binary(nums, p, left, right);
+        return answer;
+    }
+private:
+    int taken = 0;
+    int cnt = 0;
+    int mid = 0;
+    int answer = INT_MAX;
+};
+
+/*
+class Solution {
+public:
+    void binary(const vector<int> &nums, const int &p, int &left, int &right) {
+        if (left > right) return;
+        mid = (left + right) / 2;
+        cnt = 0;
         taken = -1;
         for (int i = 0; i != nums.size(); ++i) {
             if (taken == -1) {
@@ -37,6 +78,7 @@ private:
     int mid = 0;
     int answer = INT_MAX;
 };
+*/
 
 int main() {
   
