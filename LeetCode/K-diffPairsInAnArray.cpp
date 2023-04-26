5a6a340f415a90fc5e4b6bbf76ab26a61b1562ae
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-26 18:45:23 +0300

    Update K-diffPairsInAnArray.cpp

--- LeetCode/K-diffPairsInAnArray.cpp
+++ LeetCode/K-diffPairsInAnArray.cpp
@@ -9,6 +9,25 @@ public:
         unordered_map<int, int>::iterator it;
         for (const auto &e: um) {
             it = um.find(e.first + k);
+            if (it != um.cend() && ((k != 0 && it->first != e.first) || (k == 0 && it->second > 1))) {
+                ++ans;
+            } 
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int, int> um;
+        for (const auto &e: nums) {
+            um[e]++;
+        }
+        int ans = 0;
+        unordered_map<int, int>::iterator it;
+        for (const auto &e: um) {
+            it = um.find(e.first + k);
             if (it != um.cend() && it->first != e.first) {
                 ++ans;
             } else if (it != um.cend() && it->second > 1) {
@@ -18,6 +37,7 @@ public:
         return ans;
     }
 };
+*/
 
 int main() {
   
