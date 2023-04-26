ab61cb71233c0aacfbc42379d752a26d36b05a81
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-26 10:54:19 +0300

    Update CountTripletsThatCanFormTwoArraysOfEqualXOR.cpp

--- LeetCode/CountTripletsThatCanFormTwoArraysOfEqualXOR.cpp
+++ LeetCode/CountTripletsThatCanFormTwoArraysOfEqualXOR.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
@@ -11,6 +12,24 @@ public:
             answer += cnt[pref]*i - sum[pref];
             sum[pref] += i + 1;
             ++cnt[pref];
+        }
+        return answer;
+  }
+};
+*/
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int answer = 0; 
+        int pref = 0;
+        unordered_map<int, pair<int, int>> um;
+        um[0].first = 1;
+        for (int i = 0; i != arr.size(); ++i) {
+            pref ^= arr[i];
+            answer += um[pref].first*i - um[pref].second;
+            um[pref].second += i + 1;
+            ++um[pref].first;
         }
         return answer;
   }
