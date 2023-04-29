c72e3ae22a01d42cd176a2e31c4f382894e24ad4
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-29 19:08:07 +0300

    Create FindThePrefixCommonArrayOfTwoArrays.cpp

---
+++ LeetCode/FindThePrefixCommonArrayOfTwoArrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        vector<int> answer;
+        vector<bool> v1(50, 0);
+        vector<bool> v2(50, 0);
+        int cnt = 0;
+        for (int i = 0; i != A.size(); ++i) {
+            v1[A[i] - 1] = true;
+            v2[B[i] - 1] = true;
+            cnt = 0;
+            for (int j = 0; j != v1.size(); ++j) {
+                if (v1[j] == true && v2[j] == true) {
+                    ++cnt;
+                }
+            }
+            answer.push_back(cnt);
+        }
+        return answer;
+    }
+};
+
+int main() {
+  
+  
+  
+  return 0;  
+}
