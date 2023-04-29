2864a60ce37325768df06f3f13b8f236325a49d5
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-29 11:26:05 +0300

    Update EliminateMaximumNumberOfMonsters.cpp

--- LeetCode/EliminateMaximumNumberOfMonsters.cpp
+++ LeetCode/EliminateMaximumNumberOfMonsters.cpp
@@ -6,6 +6,28 @@ public:
             v.push_back(static_cast<double>(dist[i]) / speed[i]);
         }
         sort(v.begin(), v.end());
+        int start = 1;
+        int answer = 1;
+        for (int i = 1; i != v.size(); ++i) {
+            if (start < v[i]) {
+                ++start;
+                ++answer;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        vector<double> v;
+        for (int i = 0; i != dist.size(); ++i) {
+            v.push_back(static_cast<double>(dist[i]) / speed[i]);
+        }
+        sort(v.begin(), v.end());
         double start = 1;
         int answer = 1;
         for (int i = 1; i != v.size(); ++i) {
@@ -19,6 +41,7 @@ public:
         return answer;
     }
 };
+*/
 /*
 class Solution {
 public:
