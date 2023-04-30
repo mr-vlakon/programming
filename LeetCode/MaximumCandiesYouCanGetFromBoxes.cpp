019e979362f407aeea278ab24b1412a26acdb3b9
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-04-30 20:47:26 +0300

    Update MaximumCandiesYouCanGetFromBoxes.cpp

--- LeetCode/MaximumCandiesYouCanGetFromBoxes.cpp
+++ LeetCode/MaximumCandiesYouCanGetFromBoxes.cpp
@@ -10,6 +10,46 @@ public:
             ++i;
         }
         queue<int> q;
+        for (const auto &e: initialBoxes) {
+            status[e] |= 2;
+            q.push(e);
+        }
+        int answer = 0;
+        int top = 0;
+        while (!q.empty()) {
+            top = q.front();
+            q.pop();
+            for (const auto &e: keys[top]) {
+                status[e] |= true;
+            }
+            for (const auto &e: vect[top]) {
+                if (status[e] <= 1) {
+                    q.push(e);
+                    status[e] |= 2;
+                }
+            }
+        }
+        for (int i = 0; i != status.size(); ++i) {
+            if (status[i] == 3) {
+                answer += candies[i];
+            }
+        }        
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int i = 0;
+        vector<vector<int>> vect(status.size());
+        for (const auto &e: containedBoxes) {
+            for (const auto &p: e) {
+                vect[i].push_back(p);
+            }
+            ++i;
+        }
+        queue<int> q;
         vector<bool> visited(status.size(), 0);    
         for (const auto &e: initialBoxes) {
             visited[e] = true;
@@ -38,6 +78,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
   
