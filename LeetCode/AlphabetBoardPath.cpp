f518710661efdc0f6f35652c1dd674d39f0de065
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-01 22:08:37 +0300

    Update AlphabetBoardPath.cpp

--- LeetCode/AlphabetBoardPath.cpp
+++ LeetCode/AlphabetBoardPath.cpp
@@ -14,6 +14,49 @@ public:
                     answer += 'D';
                     ++startR;
                 }
+            } else {
+                while (searchR < startR) {
+                    answer += 'U';
+                    --startR;
+                }    
+            }
+            if (startC < searchC) {
+                while (startC < searchC) {
+                    answer += 'R';
+                    ++startC;
+                }
+            } else {
+                while (startC > searchC) {
+                    answer += 'L';
+                    --startC;
+                }
+            }
+            if (startR !=  searchR) {
+                answer += 'D';
+                ++startR;
+            }
+            answer += '!';
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        int startR = 0;
+        int startC = 0;
+        int searchR = 0;
+        int searchC = 0;
+        string answer;
+        for (int i = 0; i != target.size(); ++i) {
+            searchR = (target[i] - 'a') / 5;
+            searchC = (target[i] - 'a') % 5;
+            if (searchR > startR) {
+                while (searchR > startR && startR < 4) {
+                    answer += 'D';
+                    ++startR;
+                }
                 if (startR != searchR) {
                     if (startC < searchC) {
                         while (startC < searchC) {
@@ -51,6 +94,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
   
