428883ea8475360d12dc41bb320a5fba9bbe8b59
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-01 22:23:52 +0300

    Update AlphabetBoardPath.cpp

--- LeetCode/AlphabetBoardPath.cpp
+++ LeetCode/AlphabetBoardPath.cpp
@@ -10,6 +10,41 @@ public:
             searchR = (target[i] - 'a') / 5;
             searchC = (target[i] - 'a') % 5;
             if (searchR > startR) {
+                answer += string(searchR - startR - ((searchR == 5) ? 1 : 0), 'D');
+                startR = searchR - ((searchR == 5) ? 1 : 0);
+            } else {
+                answer += string(startR - searchR, 'U');
+                startR = searchR;
+            }
+            if (startC < searchC) {
+                answer += string(searchC - startC, 'R');
+                startC = searchC;
+            } else {
+                answer += string(startC - searchC, 'L');
+                startC = searchC;
+            }
+            if (startR != searchR) {
+                answer += 'D';
+                ++startR;
+            }
+            answer += '!';
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        int startR = 0;
+        int startC = 0;
+        int searchR = 0;
+        int searchC = 0;
+        string answer;
+        for (int i = 0; i != target.size(); ++i) {
+            searchR = (target[i] - 'a') / 5;
+            searchC = (target[i] - 'a') % 5;
+            if (searchR > startR) {
                 if (searchR != 5) {
                     answer += string(searchR - startR, 'D');
                     startR = searchR;
@@ -37,6 +72,7 @@ public:
         return answer;
     }
 };
+*/
 /*
 class Solution {
 public:
