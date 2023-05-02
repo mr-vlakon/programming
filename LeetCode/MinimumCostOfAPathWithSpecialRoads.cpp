5c356dd8104a95aeea44e186c163056ad3accc84
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-02 19:17:34 +0300

    Update MinimumCostOfAPathWithSpecialRoads.cpp

--- LeetCode/MinimumCostOfAPathWithSpecialRoads.cpp
+++ LeetCode/MinimumCostOfAPathWithSpecialRoads.cpp
@@ -1,3 +1,73 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) {
+        unordered_map<int, int> um;
+        int i = 1;
+        um[start[0] + 1000*start[1]] = i;
+        ++i;
+        for (const auto &e: specialRoads) {
+            if (um[e[0] + 1000*e[1]] == 0) {
+                um[e[0] + 1000*e[1]] = i;
+                ++i;
+            }
+            if (um[e[2] + 1000*e[3]] == 0) {
+                um[e[2] + 1000*e[3]] = i;
+                ++i;
+            }
+        }
+        if (um[target[0] + 1000*target[1]] == 0) {
+            um[target[0] + 1000*target[1]] = i;
+        }
+        unordered_map<int, vector<pair<int, int>>> unor;
+        for (const auto &e: specialRoads) {
+            unor[um[e[2] + 1000*e[3]]].push_back({um[target[0] + 1000*target[1]], abs(target[0] - e[2]) + abs(target[1] - e[3])});         
+            unor[um[e[0] + 1000*e[1]]].push_back({um[e[2] + 1000*e[3]], e[4]});
+            unor[um[start[0] + 1000*start[1]]].push_back({um[e[0] + 1000*e[1]], abs(e[0] - start[0]) +
+            abs(e[1] - start[1])});
+        }
+        i = 0;
+        for (; i != specialRoads.size(); ++i) {
+            for (int j = 0; j != specialRoads.size(); ++j) {
+                if (i != j) {
+                    unor[um[specialRoads[i][2] + 1000*specialRoads[i][3]]].push_back({um[specialRoads[j][0] + 1000*specialRoads[j][1]], abs(specialRoads[i][2] - specialRoads[j][0]) +
+                abs(specialRoads[i][3] - specialRoads[j][1])});
+                }
+            }
+        }
+        unor[um[start[0] + 1000*start[1]]].push_back({um[target[0] + 1000*target[1]], abs(start[0] - target[0]) + abs(start[1] - target[1])});
+        vector<bool> visited(um.size(), false);
+        priority_queue< pair<int, int>, deque<pair<int, int>>, greater<pair<int, int> >> pq;
+        pq.push({0, 1});
+        vector<int> dist(um.size(), 100000000);
+        dist[1 - 1] = 0;
+        pair<int, int> top;
+        int newDist = 0;
+        int fin = um[target[0] + 1000*target[1]];
+        while (!pq.empty()) {
+            top = pq.top();
+            pq.pop();
+            visited[top.second - 1] = true;
+            if (dist[top.second - 1] < top.first) {
+                continue;
+            }
+            for (const auto &e: unor[top.second]) {
+                if (visited[e.first - 1] == true) {
+                    continue;
+                }
+                newDist = dist[top.second - 1] + e.second;
+                if (newDist < dist[e.first - 1]) {
+                    dist[e.first - 1] = newDist;
+                    pq.push({dist[e.first - 1], e.first});
+                } 
+            }
+            if (top.second == fin) {
+                break;
+            }
+        }
+        return dist[um[target[0] + 1000*target[1]] - 1];
+    }
+};
+/*
 struct hashF {
     size_t operator ()(const pair<int, int> &p) const {
         return hash<int>{}(p.first)^hash<int>{}(p.second);
@@ -66,6 +136,7 @@ public:
         return dist[um[{target[0], target[1]}] - 1];
     }
 };
+*/
 /*
 struct hashF {
     size_t operator ()(const pair<int, int> &p) const {
