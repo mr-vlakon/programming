bbd179152e697de83aef4411c6996fb26298f6b8
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-03 13:50:01 +0300

    Update LongestPalindromeByConcatenatingTwoLetterWords.cpp

--- LeetCode/LongestPalindromeByConcatenatingTwoLetterWords.cpp
+++ LeetCode/LongestPalindromeByConcatenatingTwoLetterWords.cpp
@@ -8,6 +8,41 @@ public:
         int answer = 0;
         int rem = 0;
         string s(2, ' ');
+        unordered_map<string, int>::iterator it;
+        for (auto &e: um) {
+            s[0] = e.first[1];
+            s[1] = e.first[0];
+            it = um.find(s);
+            if ( (e.first[0] != e.first[1]) && (e.second >= 1) && (it != um.cend() && it->second >= 1)) {
+                answer += min(um[s], e.second)*4;
+                um[s] = 0;
+            } else if (e.first[0] == e.first[1]){
+                if ((e.second % 2) == 0) {
+                    answer += e.second*2;
+                } else {
+                    if (rem == 1) {
+                        answer += (e.second - 1)*2;
+                    } else {
+                        answer += e.second*2;
+                        rem = 1;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> um;
+        for (const auto &e: words) {
+            um[e]++;
+        }
+        int answer = 0;
+        int rem = 0;
+        string s(2, ' ');
         for (const auto &e: words) {
             s[0] = e[1];
             s[1] = e[0];
@@ -32,6 +67,7 @@ public:
       return answer;
     }
 };
+*/
 
 int main() {
   
