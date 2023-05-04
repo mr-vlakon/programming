1cc63ed8d29069057e451874a350dade4dbf32a0
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-04 17:39:30 +0300

    Update RabbitsInForest.cpp

--- LeetCode/RabbitsInForest.cpp
+++ LeetCode/RabbitsInForest.cpp
@@ -7,11 +7,31 @@ public:
         }
         int answer = 0;
         for (const auto &e: um) {
+            if (e.second >= (e.first + 1)) {
+                answer += (e.first + 1)*ceil((static_cast<double>(e.second) /(e.first + 1)));
+            } else {
+                answer += (e.first + 1);
+            }
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> um;
+        for (const auto &e: answers) {
+            um[e]++;
+        }
+        int answer = 0;
+        for (const auto &e: um) {
             answer += (e.first + 1)*ceil((static_cast<double>(e.second) /(e.first + 1)));
         }
         return answer;
     }
 };
+*/
 /*
 class Solution {
 public:
