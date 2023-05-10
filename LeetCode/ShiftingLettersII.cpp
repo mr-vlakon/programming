0c68845b3011bbdd26ccb0e1e3cdcd9dbbd2ad93
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-10 17:04:59 +0300

    Update ShiftingLettersII.cpp

--- LeetCode/ShiftingLettersII.cpp
+++ LeetCode/ShiftingLettersII.cpp
@@ -9,6 +9,32 @@ public:
             }
         }
         int pref = 0;
+        for (int i = v.size() - 1; i >= 0; --i) {
+            pref += v[i];
+            if (pref > 0) {
+                s[i] = ((pref + s[i] - 'a') % 26) + 'a';
+            } else {
+                s[i] = s[i] + (pref % 26);
+                if (s[i] < 'a') {
+                    s[i] += 26 ;
+                }
+            }
+        }
+        return s;
+    }
+};
+/*
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int> v(s.size(), 0);
+        for (const auto &e: shifts) {
+            v[e[1]] += (e[2] == 0) ? -1 : 1;
+            if ((e[0] - 1) >= 0) {
+                v[e[0] - 1] += (e[2] == 0) ? 1 : -1;
+            }
+        }
+        int pref = 0;
         int t = 0;
         int tmp = 0;
         for (int i = v.size() - 1; i >= 0; --i) {
@@ -32,7 +58,7 @@ public:
         return s;
     }
 };
-
+*/
 int main() {
   
   
