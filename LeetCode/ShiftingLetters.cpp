7499b334bf761d3fe97a7844fec455cc4fa4c21c
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-10 11:51:39 +0300

    Update ShiftingLetters.cpp

--- LeetCode/ShiftingLetters.cpp
+++ LeetCode/ShiftingLetters.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
+        long long sum = 0;
+        for (int i = shifts.size() - 1; i >= 0; --i) {
+            sum += shifts[i];
+            s[i] = (sum + (s[i] - 'a')) % 26 + 'a';
+        }
+        return s;
+    }
+};
+/*
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
         long long sum = accumulate(shifts.cbegin(), shifts.cend(), 0LL);
         for (int i = 0; i != shifts.size(); ++i) {
             s[i] = (sum + (s[i] - 'a')) % 26 + 'a';
@@ -9,6 +21,7 @@ public:
         return s;
     }
 };
+*/
 
 int main() {
   
