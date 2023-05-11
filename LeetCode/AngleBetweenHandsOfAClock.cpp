4d9690fa3cde8fa0e0b543d10d0520e0ac4484e7
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-11 11:00:52 +0300

    Update AngleBetweenHandsOfAClock.cpp

--- LeetCode/AngleBetweenHandsOfAClock.cpp
+++ LeetCode/AngleBetweenHandsOfAClock.cpp
@@ -7,6 +7,21 @@ public:
         h += d;
         double minV = minutes;
         minV /= 5.0;
+        double ans = (abs(h - minV) / 12.0) * 360.0;
+        if (ans > 180.0 - 0.01) return 360.0 - ans;
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double d = minutes / 60.0;
+        hour %= 12;
+        double h = hour;
+        h += d;
+        double minV = minutes;
+        minV /= 5.0;
         double diff = abs(h - (minV));
         double x = abs(h - static_cast<double>(minutes));
         double ans = (diff / 12.0) * 360.0;
@@ -14,7 +29,7 @@ public:
         return ans;
     }
 };
-
+*/
 int main() {
   
   
