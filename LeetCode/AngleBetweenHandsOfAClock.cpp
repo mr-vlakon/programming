fbb15f695fee10026e1781dfc6841cf7bf5f440f
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-11 10:28:13 +0300

    Create AngleBetweenHandsOfAClock.cpp

---
+++ LeetCode/AngleBetweenHandsOfAClock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double d = minutes / 60.0;
+        hour %= 12;
+        double h = hour;
+        h += d;
+        double minV = minutes;
+        minV /= 5.0;
+        double diff = abs(h - (minV));
+        double x = abs(h - static_cast<double>(minutes));
+        double ans = (diff / 12.0) * 360.0;
+        if (ans > 180.0 - 0.01) return 360.0 - ans;
+        return ans;
+    }
+};
+
+int main() {
+  
+  
+    
+  return 0;  
+}
