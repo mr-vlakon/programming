6cce7f03494f5f681303742c3d19ca7bd60d76ee
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-12 02:12:24 +0300

    Update CheckIfArrayPairsAreDivisibleByK.cpp

--- LeetCode/CheckIfArrayPairsAreDivisibleByK.cpp
+++ LeetCode/CheckIfArrayPairsAreDivisibleByK.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
+        vector<int> um(k, 0);
+        int t = 0;
+        for (const auto &e: arr) {
+            t = e % k;
+            if(t < 0) {
+                t += k;
+            }
+            ++um[t];
+        }
+        for (int i = 0; i != um.size(); ++i) {
+            if (um[i] == 0) continue;
+            if (i == 0) { 
+                continue;
+            }
+            if ((i * 2) == k) {
+                if ((um[i] % 2) != 0) return false;
+            }
+            if (um[i] != um[k - i]) return false;
+        }
+        return true;
+    }
+};
+/*
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
         unordered_map<int, int> um;
         int t = 0;
         for (const auto &e: arr) {
@@ -22,7 +48,7 @@ public:
         return true;
     }
 };
-
+*/
 int main() {
   
   
