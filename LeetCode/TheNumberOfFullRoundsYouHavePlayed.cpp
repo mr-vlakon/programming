c24525731ec3b4d991b408a9ab3b7fa36b4678bf
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-12 19:29:58 +0300

    Update TheNumberOfFullRoundsYouHavePlayed.cpp

--- LeetCode/TheNumberOfFullRoundsYouHavePlayed.cpp
+++ LeetCode/TheNumberOfFullRoundsYouHavePlayed.cpp
@@ -3,6 +3,24 @@ public:
     int numberOfRounds(string loginTime, string logoutTime) {
         int h1 = stoi(string(loginTime.cbegin(), loginTime.cbegin() + 3));
         int m1 = stoi(string(loginTime.cbegin() + 3, loginTime.cend()));
+        int convert = h1*60 + m1;
+        int t1 = ceil(static_cast<double>(convert) / 15.0);
+        h1 = stoi(string(logoutTime.cbegin(), logoutTime.cbegin() + 3));
+        m1 = stoi(string(logoutTime.cbegin() + 3, logoutTime.cend()));
+        convert = h1*60 + m1;
+        int t2 = floor(static_cast<double>(convert) / 15.0);
+        if (t2 < t1 && loginTime > logoutTime) {
+            return 96 - t1 + t2;
+        } else if (t2 < t1) return 0;
+        return t2 - t1;
+    }
+};
+/*
+class Solution {
+public:
+    int numberOfRounds(string loginTime, string logoutTime) {
+        int h1 = stoi(string(loginTime.cbegin(), loginTime.cbegin() + 3));
+        int m1 = stoi(string(loginTime.cbegin() + 3, loginTime.cend()));
         int convert1 = h1*60 + m1;
         h1 = stoi(string(logoutTime.cbegin(), logoutTime.cbegin() + 3));
         m1 = stoi(string(logoutTime.cbegin() + 3, logoutTime.cend()));
@@ -15,6 +33,7 @@ public:
         return t2 - t1;
     }
 };
+*/
 
 int main() {
   
