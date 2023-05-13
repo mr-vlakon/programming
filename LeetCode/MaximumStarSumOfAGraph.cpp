ec24a8be7a8e2db3c136f9c9d01c1949b9a3948e
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-13 15:49:29 +0300

    Update MaximumStarSumOfAGraph.cpp

--- LeetCode/MaximumStarSumOfAGraph.cpp
+++ LeetCode/MaximumStarSumOfAGraph.cpp
@@ -8,6 +8,32 @@ public:
             if (vals[e[0]] > 0)
                 um[e[1]].push_back(vals[e[0]]);
         }
+        for (auto &e: um) {
+            sort(e.rbegin(), e.rend());
+        }
+        int answer = INT_MIN;
+        int tmp = 0;
+        int i = 0;
+        for (const auto &e: um) {
+            tmp = vals[i] + accumulate(e.cbegin(), e.cbegin() + min(k, static_cast<int>(e.size() )), 0);
+            ++i;
+            answer = max(tmp, answer);
+        }
+        return answer;
+
+    }
+};
+/*
+class Solution {
+public:
+    int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
+        vector<vector<int>> um(vals.size());  
+        for (const auto &e: edges) {
+            if (vals[e[1]] > 0)
+                um[e[0]].push_back(vals[e[1]]);
+            if (vals[e[0]] > 0)
+                um[e[1]].push_back(vals[e[0]]);
+        }
         int i = 0;
         for (auto &e: um) {
             e.push_back(vals[i]);
@@ -26,7 +52,7 @@ public:
 
     }
 };
-
+*/
 int main() {
   
   
