7977dc473b58497ea9bd2f8e3da1dd45583a7cc9
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-13 15:47:19 +0300

    Create MaximumStarSumOfAGraph.cpp

---
+++ LeetCode/MaximumStarSumOfAGraph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
+        vector<vector<int>> um(vals.size());  
+        for (const auto &e: edges) {
+            if (vals[e[1]] > 0)
+                um[e[0]].push_back(vals[e[1]]);
+            if (vals[e[0]] > 0)
+                um[e[1]].push_back(vals[e[0]]);
+        }
+        int i = 0;
+        for (auto &e: um) {
+            e.push_back(vals[i]);
+            ++i;
+        }
+        for (auto &e: um) {
+            sort(e.rbegin() + 1, e.rend());
+        }
+        int answer = INT_MIN;
+        int tmp = 0;
+        for (const auto &e: um) {
+            tmp = e.back() + accumulate(e.cbegin(), e.cbegin() + min(k, static_cast<int>(e.size() - 1)), 0);
+            answer = max(tmp, answer);
+        }
+        return answer;
+
+    }
+};
+
+int main() {
+  
+  
+  
+  
+  return 0;  
+}
