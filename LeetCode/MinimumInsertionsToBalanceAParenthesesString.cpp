d521aaeebe8e1e53370f96279e35546a0c16ca60
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-15 19:34:42 +0300

    Update MinimumInsertionsToBalanceAParenthesesString.cpp

--- LeetCode/MinimumInsertionsToBalanceAParenthesesString.cpp
+++ LeetCode/MinimumInsertionsToBalanceAParenthesesString.cpp
@@ -22,6 +22,41 @@ public:
             if (tmpR == 2) {
                 if (tmpL == 0) {
                     ++answer;
+                } else {
+                    tmpL--;
+                }
+                tmpR = 0;
+            }
+        }
+        answer += 2*tmpL - tmpR;
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int minInsertions(string s) {
+        int answer = 0;
+        int tmpL = 0;
+        int tmpR = 0;
+        for (const auto &e: s) {
+            if (e == '(') {
+                if (tmpR != 0) {
+                    answer += 1;
+                    tmpR = 0;
+                } else {
+                    tmpL += 1;
+                }
+            } else if (e == ')') {
+                if (tmpL == 0) {
+                    ++answer;
+                    tmpL++;
+                }
+                tmpR += 1;
+            }
+            if (tmpR == 2) {
+                if (tmpL == 0) {
+                    ++answer;
                     tmpR = 0;
                 } else {
                     tmpR = 0;
@@ -33,6 +68,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
   
