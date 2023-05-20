7fc18dc56e25bea62dbec9c806a6c949b0d46e0d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-05-20 19:12:44 +0300

    Create WordLadder.cpp

---
+++ LeetCode/WordLadder.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        for (const auto &e: wordList) {
+            us.insert(e);
+        }
+        queue<pair<string, int>> q;
+        unordered_set<string> visited;
+        q.push({beginWord, 1});
+        visited.insert(beginWord);
+        int answer = INT_MAX;
+        pair<string, int> top;
+        unordered_set<string>::iterator it;
+        unordered_set<string>::iterator iter;
+        while (!q.empty()) {
+            top = q.front();
+            q.pop();
+            if (top.second >= answer) continue;
+            if (top.first == endWord) {
+                answer = min(answer, top.second);
+            }
+            for (int j = 0; j != top.first.size(); ++j) {
+                for (int i = 0; i != 26; ++i) {
+                    prev = top.first[j];
+                    top.first[j] = i + 'a';
+                    iter = visited.find(top.first);
+                    if (iter == visited.cend()) {
+                        it = us.find(top.first);
+                        if (it != us.cend()) {
+                            q.push({top.first, top.second + 1});
+                        }
+                        visited.insert(top.first);    
+                    }
+                    top.first[j] = prev;   
+                }            
+            }
+        }
+        if (answer == INT_MAX) return 0;
+        return answer;
+    }
+private:   
+    char prev;
+    unordered_set<string> us;
+};
+
+int main() {
+  
+  
+  
+  
+  return 0;  
+}
