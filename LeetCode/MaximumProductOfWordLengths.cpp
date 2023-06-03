df5067003ee57099b61d85f3e3453074020b92e8
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-06-03 17:37:12 +0300

    Update MaximumProductOfWordLengths.cpp

--- LeetCode/MaximumProductOfWordLengths.cpp
+++ LeetCode/MaximumProductOfWordLengths.cpp
@@ -10,6 +10,27 @@ public:
         }
         for (int i = 0; i != words.size() - 1; ++i) {
             for (int j = i + 1; j != words.size(); ++j) {
+                if ((vect[i]&vect[j]) == 0 && answer < (words[i].size() *words[j].size())) {
+                    answer = words[i].size()*words[j].size();
+                }
+            }
+        }   
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int answer = 0;
+        vector<int> vect(words.size(), 0);
+        for (int i = 0; i != words.size(); ++i) {
+            for (const auto &e: words[i]) {
+                vect[i] |= (1 << (e - 'a'));      
+            }
+        }
+        for (int i = 0; i != words.size() - 1; ++i) {
+            for (int j = i + 1; j != words.size(); ++j) {
                 if ((words[i].size() * words[j].size()) <= answer) continue;
                 if ((vect[i]&vect[j]) == 0) {
                     answer = max(answer, static_cast<int>(words[i].size() * words[j].size()));
@@ -19,6 +40,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
   
