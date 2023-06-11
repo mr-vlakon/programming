01e27596efb2f1fc23860b1eeed64d726db9b731
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-06-11 12:40:23 +0300

    Update CheckIfTheNumberIsFascinating.cpp

--- LeetCode/CheckIfTheNumberIsFascinating.cpp
+++ LeetCode/CheckIfTheNumberIsFascinating.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool isFascinating(int n) {
+        string result = to_string(n) + to_string(2*n) + to_string(3*n);
+        vector<int> v(10, 0);
+        for (const auto &e: result) {
+            ++v[e - '0'];
+        }
+        if (v[0] != 0) return false;
+        for (int i = 1; i != v.size(); ++i) {
+            if (v[i] != 1) return false;
+        }
+        return true;
+    }
+};
+/*
+class Solution {
+public:
+    bool isFascinating(int n) {
         string first = to_string(n);
         string second = to_string(2*n);
         string third = to_string(3*n);
@@ -16,6 +32,7 @@ public:
         return true;
     }
 };
+*/
 
 int main() {
   
