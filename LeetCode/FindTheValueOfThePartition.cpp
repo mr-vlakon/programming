3a46d70181e84b938e8a19368c54dfe6d85eee4a
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-02 09:11:24 +0300

    Create FindTheValueOfThePartition.cpp

---
+++ LeetCode/FindTheValueOfThePartition.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int minV = INT_MAX;
+        for (int i = 0; i != nums.size() - 1; ++i) {
+            if ((nums[i + 1] - nums[i]) < minV) {
+                minV = nums[i + 1] - nums[i];
+            }
+        }
+        return minV;
+    }
+};
+
+int main() {
+
+  
+  return 0;
+}
