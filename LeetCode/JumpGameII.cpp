70976fc0be3c03aa617a0ae08ccb5ad34710a4e6
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-09 11:13:17 +0300

    Update JumpGameII.cpp

--- LeetCode/JumpGameII.cpp
+++ LeetCode/JumpGameII.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        int firstReachable = nums[0];
+        int answer = 1;
+        int startInternal = 1;
+        int tmp = 0;
+        while (firstReachable < (nums.size() - 1)) {
+            if (firstReachable >= (nums.size() - 1)) return answer;
+            tmp = 0;
+            while (startInternal <= firstReachable && startInternal < nums.size()) {
+                tmp = max(tmp, max(firstReachable, startInternal + nums[startInternal]));
+                ++startInternal;
+            }
+            ++answer;
+            firstReachable = max(firstReachable, tmp);
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int jump(vector<int>& nums) {
         int reachable = 0;
         vector<int> dp(nums.size(), 100000);
         dp[0] = 0;
@@ -17,7 +39,7 @@ public:
         return dp.back();
     }
 };
-
+*/
 int main() {
 
 
