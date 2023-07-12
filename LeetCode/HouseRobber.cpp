a96c5be5b1b4c2e569d9e3839d233c9fef50085f
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-12 14:53:10 +0300

    Update HouseRobber.cpp

--- LeetCode/HouseRobber.cpp
+++ LeetCode/HouseRobber.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
+        int prevS = max(nums[0], nums[1]);
+        int prevF = nums[0];
+        int answer = prevS;
+        for (int i = 2; i != nums.size(); ++i) {
+            answer = max(prevF + nums[i], answer);
+            prevF = prevS;
+            prevS = max(prevS, answer);
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
         vector<int> dp(nums.size(), 0);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
@@ -15,6 +31,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
 
