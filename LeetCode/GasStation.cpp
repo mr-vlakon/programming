5691b3279ce34c412ac297d0d64f75acd64969e0
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-12 19:39:00 +0300

    Update GasStation.cpp

--- LeetCode/GasStation.cpp
+++ LeetCode/GasStation.cpp
@@ -3,6 +3,40 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int sum = 0;
         int cnt = 0;
+        for (int i = 0; i != gas.size(); ++i) {
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                cnt = 0;
+                sum = 0;
+            } else {
+                ++cnt;
+                if (cnt > gas.size() - 1) {
+                    return (i - cnt + 1 + gas.size()) % gas.size();
+                }
+            }   
+        }
+        for (int i = 0; i != gas.size() - 1; ++i) {
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                cnt = 0;
+                sum = 0;
+            } else {
+                ++cnt;
+                if (cnt > gas.size() - 1) {
+                    return (i - cnt + 1 + gas.size()) % gas.size();
+                }
+            }   
+        }
+        
+        return -1;
+    }
+};
+/*
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sum = 0;
+        int cnt = 0;
         for (int i = 0; i != 2*gas.size() - 1; ++i) {
             sum += gas[i % gas.size()] - cost[i % gas.size()];
             if (sum < 0) {
@@ -18,7 +52,7 @@ public:
         return -1;
     }
 };
-
+*/
 int main() {
 
 
