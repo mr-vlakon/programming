78a12214e8fefa633e2860592f9aa01809c49c0d
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-20 20:37:14 +0300

    Update 2KeysKeyboard.cpp

--- LeetCode/2KeysKeyboard.cpp
+++ LeetCode/2KeysKeyboard.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     int minSteps(int n) {
         vector<int> v(n, 0);
+        for (int i = 1; i != n; ++i) {
+            for (int j = i / 2; j >= 0; --j) {
+                if (((i + 1) % (j + 1)) == 0)
+                {
+                    v[i] = v[j] + (i + 1) / (j + 1);
+                    break;
+                }
+            }
+        }
+        return v.back(); 
+    }
+};
+/*
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> v(n, 0);
         int tmp = 0;
         for (int i = 1; i != n; ++i) {
             tmp = INT_MAX;
@@ -16,6 +33,7 @@ public:
         return v.back(); 
     }
 };
+*/
 
 int main() {
 
