9859bc18d4ba3442f7845ee3d479e961ccd007a5
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2023-07-31 10:27:22 +0300

    Update MaximumErasureValue.cpp

--- LeetCode/MaximumErasureValue.cpp
+++ LeetCode/MaximumErasureValue.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
+        vector<int> sums(nums.size(), 0);
+        vector<int> um(10001, -1);
+        int sum = 0;
+        int answer = 0;
+        int ind = -1;
+        unordered_map<int, int>::iterator it;
+        for (int i = 0; i != nums.size(); ++i) {
+            sum += nums[i];
+            if (um[nums[i]] == -1) {
+                if (ind == -1) {
+                    answer = max(answer, sum);
+                } else {
+                    answer = max(answer, sum - sums[ind]);
+                }
+            } else {
+                ind = max(ind, um[nums[i]]);
+                answer = max(answer, sum - sums[ind]);
+            }
+            um[nums[i]] = i; 
+            sums[i] = sum;
+        }
+        return answer;
+    }
+};
+/*
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
         unordered_map<int, int> um;
         vector<int> sums(nums.size(), 0);
         int sum = 0;
@@ -26,6 +54,7 @@ public:
         return answer;
     }
 };
+*/
 
 int main() {
 
