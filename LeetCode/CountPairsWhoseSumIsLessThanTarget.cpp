d5494673b03725d5544e7ec3312a60ddf022f39a
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2024-01-04 12:37:30 +0300

    Create CountPairsWhoseSumIsLessThanTarget.cpp

---
+++ LeetCode/CountPairsWhoseSumIsLessThanTarget.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int answer = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if ((nums[i] + nums[j]) < target) {
+                    ++answer;
+                }
+            }
+        }
+        return answer;
+    }
+};
+
+int main()
+{
+  
+  return 0;
+}
