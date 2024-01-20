ca5fc5c15f5c2c6dd6f65a9d560a80aff1e514cd
mr-vlakon <112072140+mr-vlakon@users.noreply.github.com>
2024-01-20 16:43:43 +0300

    Update DetermineTheMinimumSumOfAK-AvoidingArray.cpp

--- LeetCode/DetermineTheMinimumSumOfAK-AvoidingArray.cpp
+++ LeetCode/DetermineTheMinimumSumOfAK-AvoidingArray.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void generate(const int &n, const int &k, int &sum, int cnt, vector<bool> &um) {
@@ -47,6 +48,18 @@ public:
     }
 private:
     int answer = 0;
+};
+*/
+// better solution
+class Solution {
+public:
+    int minimumSum(int n, int k) {
+        int target = k;
+        if (n <= (target / 2)) return (n*(n + 1) / 2) % 1000000007;
+        int half = target / 2;
+        int finish = target + n - half - 1;
+        return (static_cast<long long>(half)*(half + 1) / 2 + static_cast<long long>(finish)*(finish + 1) / 2 - static_cast<long long>(target)*(target - 1) /2) % 1000000007;
+    }
 };
 
 int main() 
